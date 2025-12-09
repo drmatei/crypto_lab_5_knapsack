ee4894f4700c333c66ab77b16b169d0bb5279860
halip-sonia <sonia.halip@stud.ubbcluj.ro>
2025-12-09 19:34:57 +0200

    Decrypt function + added decryption to main

--- main.py
+++ main.py
@@ -100,12 +100,59 @@ class KnapsackCrypto:
 
         return ciphertext_blocks
 
+    def subset_sum_problem(self, w, c_prime):
+        """
+        Greedy algorithm that obtains a string of bits, with the 1 bits representing the largest elements of the
+        superincreasing sequence w that sum up to c_prime.
+        """
+        bit_result = ''
+
+        # Since w is a superincreasing sequence, the list is reversed to obtain the largest elements first
+        for w_element in reversed(w):
+            if c_prime - w_element >= 0:
+                bit_result = '1' + bit_result
+                c_prime -= w_element
+            else:
+                bit_result = '0' + bit_result
+            # The list is reversed, so we add the bits to the front of the bit_result string
+        return bit_result
+
+    def decrypt(self, cyphertext, private_key):
+        """
+        Using the private key, decrypts the cyphertext
+        """
+        w, q, r = private_key
+
+        # 1. Calculate the modular inverse of the modular inverse of r modulo q
+        r_prime = pow(r, -1, q)
+
+        bit_string = ''
+        for cyphertext_block in cyphertext:
+            # 2. For each block in the cyphertext, calculate c * r_prime mod q
+            c_prime = (cyphertext_block * r_prime) % q
+
+            # 3. For each block, we resolve the subset sum problem using the superincreasing sequence w,
+            # and add its result to the binary message string
+            string_result = self.subset_sum_problem(w, c_prime)
+            bit_string += string_result
+
+        decrypted = ''
+        for i in range(0, len(bit_string), 5):
+            # 4. We reconstruct the original message by splitting the binary message string into chunks of 5
+            # (since 27 characters need 5 bits), and attributing them a character from the defined alphabet
+            chunk = ''.join(map(str, bit_string[i:i + 5]))
+            chunk_integer = int(chunk, 2)
+            decrypted += self.int_to_char[chunk_integer]
+
+        return decrypted
+
 
 # example use for testing
 if __name__ == "__main__":
     crypto = KnapsackCrypto()
 
-    # Generate Keys (the size,n, is the number of bits per block, ideally multiple of 5 since n / 5 will be the number of chars in each block)
+    # Generate Keys (the size,n, is the number of bits per block, ideally multiple of 5 since n / 5 will be the
+    # number of chars in each block)
     pub_key, priv_key = crypto.generate_keys(n=10)
 
     print(f"Public Key: {pub_key}")
@@ -114,9 +161,12 @@ if __name__ == "__main__":
     message = input("\nEnter plaintext (Space and A-Z only): ")
     print(f"\nOriginal Message: '{message}'")
 
-    # Encrypt
+    # Encrypt and Decrypt
     try:
         cipher = crypto.encrypt(message, pub_key)
-        print(f"Encrypted Ciphertext: {cipher}")
+        print(f"\nEncrypted Ciphertext: {cipher}")
+
+        decrypted_message = crypto.decrypt(cipher, priv_key)
+        print(f"\nDecrypted Ciphertext: {decrypted_message}")
     except ValueError as e:
         print(f"Error: {e}")
